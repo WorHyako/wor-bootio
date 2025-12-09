269bfab8a4ea090af26984d60e3f94f783928287
WorHyako <worhyako@gmail.com>
2025-12-09 11:30:10 +0100

    cmock + unity tools; doxygen; target for testing

---
+++ cmake/ThirdPartyDeps.cmake
@@ -0,0 +1,28 @@
+include(FetchContent)
+
+# --------- #
+#   Unity   #
+# --------- #
+FetchContent_Declare(unity
+        GIT_REPOSITORY https://github.com/ThrowTheSwitch/Unity.git
+        GIT_TAG cbcd08fa7de711053a3deec6339ee89cad5d2697 # v2.6.1
+        OVERRIDE_FIND_PACKAGE)
+
+FetchContent_MakeAvailable(unity)
+
+# --------- #
+#   CMock   #
+# --------- #
+FetchContent_Declare(cmock
+        GIT_REPOSITORY https://github.com/ThrowTheSwitch/CMock.git
+        GIT_TAG 49435d9f3313abb3422252d3d2dc12a7542332ca # v2.6.0
+        OVERRIDE_FIND_PACKAGE)
+
+FetchContent_MakeAvailable(cmock)
+
+add_library(cmock INTERFACE)
+
+target_include_directories(cmock
+        INTERFACE ${cmock_SOURCE_DIR}/src)
+
+add_library(cmock::cmock ALIAS cmock)
---
+++ tests/unit/test_upload.c
@@ -0,0 +1,22 @@
+#include "bootio_error.h"
+#include "unity.h"
+#include "load.h"
+#include "configuration.h"
+#include "dfu_file.h"
+
+#include "mock_transfer.h"
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    // RUN_TEST();
+    return UNITY_END();
+}
